5d61e1a6ee02267133a0bd832c44c037e35b0c36
HILARION <hilarionium@gmail.com>
2021-07-08 13:07:13 +0300

    Connected to the database and created routing

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
+import { Request, Response } from 'express';
+
 const express = require('express');
 
-const PORT = process.env.PORT || 8088;
+const PORT = 8080;
 const app = express();
+
+app.post('/upload/dog/image', async (req: Request, res: Response) => {
+  res.status(200).send('post');
+});
+
+app.get('/list/dog/images', async (req: Request, res: Response) => {
+  res.status(200).send('get');
+});
 
 app.listen(PORT, () => {
   console.log('Server do work!');
